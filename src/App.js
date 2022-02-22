0aff2ce68e9f6e7e711bd0302ce0e9939540a42f
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-22 12:17:03 +0700

    feat: add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button'
+import React from 'react'
 
 function App() {
+  const [toggle, setToggle] = React.useState(true)
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {toggle && <Button sapa = "Hello" />}
+          <button onClick={()=>setToggle(false)}>Hide </button>
         </p>
         <a
           className="App-link"
