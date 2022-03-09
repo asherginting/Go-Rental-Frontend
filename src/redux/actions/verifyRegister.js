887b35cdf01530b0c47d71a99a5a1eea34a7e1c1
Asher Azriel Ginting <asherginting3@gmail.com>
2022-03-09 09:20:47 +0700

    feat: add action verf. Signup

---
+++ src/redux/actions/verifyRegister.js
@@ -0,0 +1,12 @@
+import http from "../../helper/http";
+
+export const verifyRegister = (username, code, password) => {
+  const param = new URLSearchParams()
+  param.append('username', username)
+  param.append('code', code)
+  param.append('password', password)
+  return ({
+    type: 'VERIFY_REGISTER',
+    payload: http().post('/auth/verification', param)
+  })
+}
