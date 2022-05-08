1c4cef4880ed37eebcfb071adbd8f7f1f4c1b093
Asher Azriel Ginting <asherginting3@gmail.com>
2022-05-08 21:44:48 +0700

    feat: additem action

---
+++ src/redux/actions/addItems.js
@@ -0,0 +1,17 @@
+import http from '../../helper/http';
+
+export const addVehicle = (data) =>{
+  const params = new FormData();
+  const token = window.localStorage.getItem('token');
+  params.append ('id_category', data.id_category);
+  params.append ('brand', data.brand);
+  params.append ('image', data.image);
+  params.append ('capacity', data.capacity);
+  params.append ('location', data.location);
+  params.append ('price', data.price);
+  params.append ('qty', data.qty);
+  return({
+    type:'VEHICLE_INSERT',
+    payload: http(token).post('/vehicles', params)
+  });
+};
