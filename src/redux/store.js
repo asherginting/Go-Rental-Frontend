8a2bca2a7fa20511d4e56ef13888c289e3465412
Asher Azriel Ginting <asherginting3@gmail.com>
2022-05-13 19:54:26 +0700

    fix: configure persiststore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import promise from 'redux-promise-middleware';
+import {persistStore} from 'redux-persist';
 import rootReducer from './reducers';
 
-const store = createStore (
-    rootReducer,
-    applyMiddleware(
-        promise,
-        logger,
-    )
-);
-
-export default store;
+export default () => {
+    const store = createStore(rootReducer, applyMiddleware(promise, logger));
+    const persistor = persistStore(store);
+    return {store, persistor};
+};
