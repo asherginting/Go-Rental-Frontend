294fb159f9d82a3f8da8e411ecd7b5bcc864d81d
Asher Azriel Ginting <asherginting3@gmail.com>
2022-05-13 23:59:59 +0700

    fix: sort, search and pagination

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,83 +1,177 @@
-/* eslint-disable react/no-unescaped-entities */
-import React, { useEffect, useState } from 'react';
+// /* eslint-disable react/no-unescaped-entities */
+// import React, { useEffect } from 'react';
+// import '../assets/css/vehicle-type.css';
+// import Vehicle from '../components/Vehicle';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useNavigate, useSearchParams } from 'react-router-dom';
+// import { searchVehicle, nextSearchVehicle } from '../redux/actions/vehicle';
+// import searchURL from '../helper/searchURL';
+// import {BiSearchAlt2} from 'react-icons/bi';
+// import LoadingSkeleton from '../components/LoadingSkeleton';
+
+// export default function Search() {
+//     const key = searchParams.get('keyword') || '';
+//     const location = searchParams.get('location') || '';
+//     const min = searchParams.get('min') || 0;
+//     const max = searchParams.get('max') || 100000000;
+
+//     const { search } = useSelector((state) => state.vehicleReducer);
+//     const dispatch = useDispatch();
+
+//     const navigate = useNavigate();
+//     const [searchParams] = useSearchParams();
+
+//     useEffect(() => {
+//         window.scrollTo(0, 0);
+//         dispatch(searchVehicle(key, location, min, max));
+//     }, [searchParams]);
+
+//     const nextPage = () => {
+//         dispatch(nextSearchVehicle(key, location, min, max, search.pageInfo.currentPage + 1));
+//     };
+//     const handleSubmit = (ev) => {
+//         ev.preventDefault();
+//         navigate(searchURL(ev));
+//     };
+
+//     return (
+//         <div className='vehicle-type'>
+//             <section className='container'>
+//                 <form onSubmit={handleSubmit} className="container d-flex position-relative">
+//                     <input className="form-control" name='brand' type="search" placeholder="Search vehicle name" />
+//                     <input className="form-control" name='location' type="search" placeholder="Location" />
+//                     <input className="form-control" name='minimal' type="number" placeholder="Min. Price" />
+//                     <input className="form-control" name='maximal' type="number" placeholder="Max. Price" />
+//                     <button type='submit' className="btn login">
+//                         {' '}
+//                         <i className="search-icon"><BiSearchAlt2 /></i>
+//                     </button>
+//                 </form>
+//                 <div className="head">
+//                     {search.vehicle.length > 0 ? <p className="text-muted text-center">Click item to see details and reservation</p> 
+//                         : <p className="text-center text-muted py-5">
+//                             did not match any document
+//                         </p>}
+//                 </div>
+//                 <div className='row'>
+//                     {search.isLoading && <LoadingSkeleton count={8} />}
+//                     {search.vehicle?.map(data => {
+//                         const props = {
+//                             image: data.image, location: data.location, brand: data.brand, id: data.idVehicle,
+//                         };
+//                         if (data.qty > 0) return <Vehicle key={props.id} props={props} />;
+//                     })}
+//                 </div>
+//                 <div className='my-4 text-center'>
+//                     {search.vehicle.length > 0 
+//                         && (search.pageInfo.next ?
+//                             <button onClick={nextPage} className='btn btn-green w-25'>Load More</button> :
+//                             <p className="text-center text-muted py-5">There is no vehicle left</p>   
+//                         )
+//                     }
+//                 </div>
+        
+//             </section>
+//         </div>
+//     );
+// }
+
+import React, { useEffect } from 'react';
 import '../assets/css/vehicle-type.css';
-import Vehicle from '../components/Vehicle';
-import {default as axios} from 'axios';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import {BiSearchAlt2} from 'react-icons/bi';
+import { BiSearchAlt2 } from 'react-icons/bi';
+import { useDispatch, useSelector } from 'react-redux';
+import Vehicle from '../components/Vehicle';
+import searchURL from '../helper/searchURL';
+import { searchVehicle, nextSearchVehicle } from '../redux/actions/vehicle';
+import LoadingSkeleton from '../components/LoadingSkeleton';
 
 export default function Search() {
-    const [vehicle, setVehilcle] = useState([]);
-    const [page, setPage] = useState({});
-  
+    const { search } = useSelector((state) => state.vehicleReducer);
+    const dispatch = useDispatch();
+
     const navigate = useNavigate();
     const [searchParams] = useSearchParams();
 
+    const key = searchParams.get('keyword') || '';
+    const location = searchParams.get('location') || '';
+    const min = searchParams.get('min') || 0;
+    const max = searchParams.get('max') || 100000000;
+
     useEffect(() => {
         window.scrollTo(0, 0);
-        const params = searchParams.get('type') ? {type: searchParams.get('type')} 
-            : {search: searchParams.get('keyword') || '', filter: searchParams.get('filter') || ''};
-        getVehicle(params);
+        dispatch(searchVehicle(key, location, min, max));
     }, [searchParams]);
 
-    const getVehicle = async (params) => {
-        const url = params.search ?
-            (params.type ? 
-                `http://localhost:5000/popular?limit=8&search=${params.type}` 
-                : `http://localhost:5000/vehicles/category/?search=${params.search}&filter=${params.filter}&limit=8`) 
-            : 'http://localhost:5000/popular?limit=8';
-        const {data} = await axios.get(url);
-        setVehilcle(data.results); 
-        setPage(data.pageInfo); 
+    const nextPage = () => {
+        dispatch(nextSearchVehicle(key, location, min, max, search.pageInfo.currentPage + 1));
     };
-
-    const nextPage = async () => {
-        try {
-            const {data} = await axios.get(page.next);
-            setVehilcle([...vehicle, ...data.results]);
-            setPage(data.pageInfo);
-        } catch (err) {
-            console.log(err.message);
-        }
+    const beforePage = () => {
+        dispatch(nextSearchVehicle(key, location, min, max, search.pageInfo.currentPage - 1));
     };
     const handleSubmit = (ev) => {
         ev.preventDefault();
-        const key = ev.target.elements['search'].value;
-        const fil = ev.target.elements['filter'].value;
-        navigate(fil ? `/search?keyword=${key}&filter=${fil}` : `/search?keyword=${key}`);
+        navigate(searchURL(ev));
     };
 
     return (
-        <div className='vehicle-type'>
-            <section className='container'>
+        <div className="vehicle-type">
+            <section className="form-search">
                 <form onSubmit={handleSubmit} className="container d-flex position-relative">
-                    <input className="form-control" name='search' type="search" placeholder="Search vehicle (ex. cars name)" />
-                    <input className="form-control" name='filter' type="search" placeholder="Filter (ex. location)" />
-                    <button type="submit" className="btn position-absolute end-0" aria-label="search button">
+                    <div className="col-md-3">
+                        <input className="form-control" name="brand" type="search" placeholder="Vehicle Name" />
+                    </div>
+                    <div className="col-md-3">
+                        <input className="form-control" name="location" type="search" placeholder="Loc. Vehicle" />
+                    </div>
+                    <div className="col-md-3">
+                        <input className="form-control" name="minimum" type="number" placeholder="Min. Price Vehicle" />
+                    </div>
+                    <div className="col-md-3">
+                        <input className="form-control" name="maximum" type="number" placeholder="Max. Price Vehicle" />
+                    </div>
+                    {/* <div className="col-md-4">
+                        <input className="form-control my-2" name="minimum" type="number" placeholder="Min price" />
+                    </div> */}
+                    {/* <div className="col-md-4">
+                        <input className="form-control my-2" name="maximum" type="number" placeholder="Max price" />
+                    </div> */}
+                    <button type='submit' className="btn login d-flex position-relative">
+                        {' '}
                         <i className="search-icon"><BiSearchAlt2 /></i>
                     </button>
                 </form>
-                <div className="head">
-                    {vehicle.length > 0 ? <p className="text-muted text-center">Click item to see details and reservation</p> 
-                        : <p className="text-center text-muted py-5">
-            Your search '{searchParams.get('keyword')} {searchParams.get('filter')}' did not match any document
-                        </p>}
+            </section>
+            <section className="container">
+                <div className="head mt-3">
+                    {search.vehicle.length > 0 ? <p className="text-muted text-center">Click item to see details and reservation</p>
+                        : (
+                            <p className="text-center text-muted py-5">
+                Did not find search results
+                            </p>
+                        )}
                 </div>
-                <div className='row'>
-                    {vehicle.map(data => {
-                        const props = {image: data.image, location: data.location, brand: data.brand, id: data.idVehicle};
-                        return <Vehicle key={props.id} props={props} />;
+                <div className="row">
+                    {search.isLoading && <LoadingSkeleton count={8} />}
+                    {search.vehicle?.map((data) => {
+                        const props = {
+                            image: data.image, location: data.location, brand: data.brand, id: data.idVehicle,
+                        };
+                        if (data.qty > 0) return <Vehicle key={props.id} props={props} />;
                     })}
                 </div>
-                <div className='my-4 text-center'>
-                    {vehicle.length > 0 ?
-                        (page.next ?
-                            <button onClick={nextPage} className='btn btn-green w-25'>Load More</button> :
-                            <p className="text-center text-muted py-5">There is no vehicle left</p>   
-                        ) : <></>
-                    }
+                <div className="my-4 text-end">
+                    {search.vehicle.length > 0 && (search.pageInfo.next? 
+                        <button onClick={beforePage} className="btn btn-green w-10" type="button">1</button>
+                        : <p className="text-center text-muted py-5">There is no vehicle left</p>
+                    )}
+                    {search.vehicle.length > 0 && (search.pageInfo.next? 
+                        <button onClick={nextPage} className="btn btn-green w-10" type="button">2</button>
+                        : <button onClick={beforePage} className="btn btn-green w-10" type="button">1</button>
+                        
+                    )}
                 </div>
-        
+
             </section>
         </div>
     );
--- src/pages/VehicleType.js
+++ src/pages/VehicleType.js
@@ -1,15 +1,92 @@
-import React, { useEffect} from 'react';
+// import React, { useEffect} from 'react';
+// import '../assets/css/vehicle-type.css';
+// import Vehicle from '../components/Vehicle';
+// import {BiSearchAlt2} from 'react-icons/bi';
+// import {IoChevronForward} from 'react-icons/io5';
+// import { Link, useNavigate } from 'react-router-dom';
+// import { category, popular as popularVehicle } from '../redux/actions/vehicle';
+// import { useDispatch, useSelector } from 'react-redux';
+// import searchURL from '../helper/searchURL';
+// import LoadingSkeleton from '../components/LoadingSkeleton';
+
+// const VehicleType = () => {
+//     const { popular } = useSelector((state) => state.vehicleReducer);
+//     const { cars } = useSelector((state) => state.vehicleReducer);
+//     const { motorbike } = useSelector((state) => state.vehicleReducer);
+//     const { bike } = useSelector((state) => state.vehicleReducer);
+
+//     const dispatch = useDispatch();
+
+//     const navigate = useNavigate();
+
+//     useEffect(() => {
+//         window.scrollTo(0, 0);
+//         dispatch(popularVehicle());
+//         dispatch(category('CARS', 'cars'));
+//         dispatch(category('MOTORBIKE', 'motorbike'));
+//         dispatch(category('BIKE', 'bike'));
+//     }, []);
+
+//     const handleSubmit = (ev) => {
+//         ev.preventDefault();
+//         navigate(searchURL(ev));
+//     };
+
+//     const product = (head, state, type) => (
+//         <section className="container" id="parrent">
+//             <div className="d-flex justify-content-between head">
+//                 <h2>{head}</h2>
+//                 <Link to={type ? `/vehicle?type=${type}` : '/vehicle'} className="view-all">
+//           View all
+//                     <IoChevronForward />
+//                 </Link>
+//             </div>
+//             <div className="row mb-5">
+//                 {state.isLoading && <LoadingSkeleton count="4" />}
+//                 {state.vehicle.map((data, index) => {
+//                     const props = {
+//                         image: data.image, location: data.location, brand: data.brand, id: data.idVehicle,
+//                     };
+//                     return (index < 4 && <Vehicle key={props.id} props={props} />);
+//                 })}
+//             </div>
+//         </section>
+//     );
+
+//     return (      
+//         <div className='vehicle-type'>
+//             <form onSubmit={handleSubmit} className="container d-flex position-relative">
+//                 <input className="form-control" name='brand' type="search" placeholder="Search vehicle name" />
+//                 <input className="form-control" name='location' type="search" placeholder="Location" />
+//                 <input className="form-control" name='minimal' type="number" placeholder="Min. Price" />
+//                 <input className="form-control" name='maximal' type="number" placeholder="Max. Price" />
+//                 <button type='submit' className="btn login">
+//                     {' '}
+//                     <i className="search-icon"><BiSearchAlt2 /></i>
+//                 </button>
+//             </form>
+//             {product('Popular in town', popular)}
+//             {product('Cars', cars, 'cars')}
+//             {product('Motorbike', motorbike, 'motorbike')}
+//             {product('Bike', bike, 'Bike')}
+//         </div>
+//     );
+// };
+
+// export default VehicleType;
+
+import React, { useEffect } from 'react';
 import '../assets/css/vehicle-type.css';
-import Vehicle from '../components/Vehicle';
-import {BiSearchAlt2} from 'react-icons/bi';
-import {IoChevronForward} from 'react-icons/io5';
+import { BiSearchAlt2 } from 'react-icons/bi';
+import { IoChevronForward } from 'react-icons/io5';
 import { Link, useNavigate } from 'react-router-dom';
-import { category, popular as popularVehicle } from '../redux/actions/vehicle';
 import { useDispatch, useSelector } from 'react-redux';
 import searchURL from '../helper/searchURL';
+import Vehicle from '../components/Vehicle';
+import { category, popular as popularVehicle } from '../redux/actions/vehicle';
 import LoadingSkeleton from '../components/LoadingSkeleton';
 
-const VehicleType = () => {
+function VehicleType() {
     const { popular } = useSelector((state) => state.vehicleReducer);
     const { cars } = useSelector((state) => state.vehicleReducer);
     const { motorbike } = useSelector((state) => state.vehicleReducer);
@@ -25,6 +102,7 @@ const VehicleType = () => {
         dispatch(category('CARS', 'cars'));
         dispatch(category('MOTORBIKE', 'motorbike'));
         dispatch(category('BIKE', 'bike'));
+        dispatch(category('PICKUP', 'pickup'));
     }, []);
 
     const handleSubmit = (ev) => {
@@ -32,44 +110,63 @@ const VehicleType = () => {
         navigate(searchURL(ev));
     };
 
-    const product = (head, state, type) => (
-        <section className="container" id="parrent">
-            <div className="d-flex justify-content-between head">
-                <h2>{head}</h2>
-                <Link to={type ? `/vehicle?type=${type}` : '/vehicle'} className="view-all">
-          View all
-                    <IoChevronForward />
-                </Link>
-            </div>
-            <div className="row mb-5">
-                {state.isLoading && <LoadingSkeleton count="4" />}
-                {state.vehicle.map((data, index) => {
-                    const props = {
-                        image: data.image, location: data.location, brand: data.brand, id: data.idVehicle,
-                    };
-                    return (index < 4 && <Vehicle key={props.id} props={props} />);
-                })}
-            </div>
-        </section>
-    );
+    function Product({ head, state, type }) {
+        return (
+            <section className="container" id="parrent">
+                <div className="d-flex justify-content-between head">
+                    <h2>{head}</h2>
+                    <Link to={type ? `/vehicle?type=${type}` : '/vehicle'} className="view-all">
+            View all
+                        <IoChevronForward />
+                    </Link>
+                </div>
+                <div className="row mb-5">
+                    {state.isLoading && <LoadingSkeleton count="4" />}
+                    {state.vehicle?.map((data, index) => {
+                        const props = {
+                            image: data.image, location: data.location, brand: data.brand, id: data.idVehicle,
+                        };
+                        return (index < 4 && <Vehicle key={props.id} props={props} />);
+                    })}
+                </div>
+            </section>
+        );
+    }
 
-    return (      
-        <div className='vehicle-type'>
-            <form onSubmit={handleSubmit} className="container d-flex position-relative">
-                <input className="form-control" name='search' type="search" placeholder="Search vehicle name" />
-                <input className="form-control" name='location' type="search" placeholder="Location" />
-                <input className="form-control" name='min' type="number" placeholder="Min. Price" />
-                <input className="form-control" name='max' type="number" placeholder="Max. Price" />
-                <button type='submit' className="btn login">
-                    <i className="search-icon"><BiSearchAlt2 /></i>
-                </button>
-            </form>
-            {product('Popular in town', popular)}
-            {product('Cars', cars, 'cars')}
-            {product('Motorbike', motorbike, 'motorbike')}
-            {product('Bike', bike, 'Bike')}
+    return (
+        <div className="vehicle-type">
+            <section className="form-search">
+                <form onSubmit={handleSubmit} className="container d-flex position-relative">
+                    <div className="col-md-3">
+                        <input className="form-control" name="brand" type="search" placeholder="Vehicle Name" />
+                    </div>
+                    <div className="col-md-3">
+                        <input className="form-control" name="location" type="search" placeholder="Loc. Vehicle" />
+                    </div>
+                    <div className="col-md-3">
+                        <input className="form-control" name="minimum" type="number" placeholder="Min. Price Vehicle" />
+                    </div>
+                    <div className="col-md-3">
+                        <input className="form-control" name="maximum" type="number" placeholder="Max. Price Vehicle" />
+                    </div>
+                    {/* <div className="col-md-4">
+                        <input className="form-control my-2" name="minimum" type="number" placeholder="Min price" />
+                    </div> */}
+                    {/* <div className="col-md-4">
+                        <input className="form-control my-2" name="maximum" type="number" placeholder="Max price" />
+                    </div> */}
+                    <button type='submit' className="btn login d-flex position-relative">
+                        {' '}
+                        <i className="search-icon"><BiSearchAlt2 /></i>
+                    </button>
+                </form>
+            </section>
+            <Product head="Popular in town" state={popular} />
+            <Product head="Cars" state={cars} type="cars" />
+            <Product head="Motorbike" state={motorbike} type="motorbike" />
+            <Product head="Bike" state={bike} type="bike" />
         </div>
     );
-};
+}
 
 export default VehicleType;
